36af803da27a0bf859b9115091dc28597035bfca
Maximus7474 <the.maximus.7474@gmail.com>
2024-11-10 11:36:53 +0100

    chore: update base.sql -> changed the id to vin

--- base.sql
+++ base.sql
@@ -1,37 +1,13 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               11.4.3-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             12.5.0.6677
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- Dumping structure for table ox_core_1ab13f.vehicles_persist
 CREATE TABLE IF NOT EXISTS `vehicles_persist` (
-  `vin` varchar(17) NOT NULL,
-  `location_x` int(3) NOT NULL,
-  `location_y` int(3) NOT NULL,
-  `location_z` int(3) NOT NULL,
-  `location_h` int(3) NOT NULL,
-  `rotation_x` int(3) NOT NULL,
-  `rotation_y` int(3) NOT NULL,
-  `rotation_z` int(3) NOT NULL,
-  KEY `vin_fk` (`vin`),
-  CONSTRAINT `vin_fk` FOREIGN KEY (`vin`) REFERENCES `vehicles` (`vin`) ON DELETE CASCADE ON UPDATE NO ACTION
+  `id` int(10) unsigned NOT NULL,
+  `location_x` float NOT NULL DEFAULT 0,
+  `location_y` float NOT NULL DEFAULT 0,
+  `location_z` float NOT NULL DEFAULT 0,
+  `rotation_x` float NOT NULL DEFAULT 0,
+  `rotation_y` float NOT NULL DEFAULT 0,
+  `rotation_z` float NOT NULL DEFAULT 0,
+  KEY `vehicleId_FK1` (`id`),
+  CONSTRAINT `vehicleId_FK1` FOREIGN KEY (`id`) REFERENCES `vehicles` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
-
--- Data exporting was unselected.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
