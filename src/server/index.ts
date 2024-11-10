06130846194ae7f4860ce60c5544fc2d3fb68722
Maximus7474 <the.maximus.7474@gmail.com>
2024-11-10 11:42:31 +0100

    rf(server): added debug func for logging

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,12 @@ import { GetVehicle, SpawnVehicle, OxVehicle } from "@overextended/ox_core/serve
 import { oxmysql as MySQL } from '@overextended/oxmysql';
 import { persistedVehicle } from "./types";
 
+const dev = GetConvarInt('ox:debug', 0) === 1;
+const DEBUG = (...args: any[]): void => {
+  if (!dev) return;
+  console.log(`[^4${GetCurrentResourceName()}^7]`, ...args);
+};
+
 on('onResourceStop', async (resource: string) => {
   if (resource !== GetCurrentResourceName()) return;
 
@@ -20,14 +26,14 @@ on('onResourceStop', async (resource: string) => {
     try {
       MySQL.insert('INSERT INTO `vehicles_persist` (id, location_x, location_y, location_z, rotation_x, rotation_y, rotation_z) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [
         vehicle.id, coords[0], coords[1], coords[2], rotation[0], rotation[1], rotation[2]
-      ], console.log);
+      ], DEBUG);
 
       vehicle.setStored('parked');    
       vehicle.despawn(true);
 
       saved++;
     } catch (err: any) {
-      console.log('Unable to save', vehicle.id, vehicle.plate, 'to DB', err.message);
+      DEBUG('Unable to save', vehicle.id, vehicle.plate, 'to DB', err.message);
     }
   };
 
@@ -36,20 +42,20 @@ on('onResourceStop', async (resource: string) => {
 
 
 setTimeout(async () => {
-  console.log(`[${GetCurrentResourceName()}] Respawning vehicles !`);
+  DEBUG(`Respawning vehicles`);
   MySQL.query('SELECT * FROM `vehicles_persist`', async (vehicles: persistedVehicle[]) => {
-    console.log('Respawning', vehicles.length, 'vehicles');
+    DEBUG('Respawning', vehicles.length, 'vehicles');
     vehicles.forEach(async (vehicleData) => {
       const { id, location_x, location_y, location_z, rotation_x, rotation_y, rotation_z } = vehicleData;
   
       SpawnVehicle(id, [location_x, location_y, location_z + 0.98], rotation_z)
       .then(vehicle => {
-        if (!vehicle) return;
+        if (!vehicle) return DEBUG(`Vehicle ${id} was not created!`);
         SetEntityRotation(vehicle.entity, rotation_x, rotation_y, rotation_z, 0, false);
       });  
     });
 
     MySQL.update('DELETE FROM `vehicles_persist`');
-    console.log(`[${GetCurrentResourceName()}] Respawned all vehicles`);
+    DEBUG(`Respawned all vehicles`);
   });
 }, 1000);
