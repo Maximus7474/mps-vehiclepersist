02c184cfbeb931ff51ec4dcbcfdab32339a27d90
Maximus7474 <the.maximus.7474@gmail.com>
2024-11-13 10:20:52 +0100

    rf(build): added ox_core and ox_lib as dependancies

--- scripts/build.js
+++ scripts/build.js
@@ -25,7 +25,7 @@ createBuilder(
     await createFxmanifest({
       server_scripts: [outfiles.server],
       files: files,
-      dependencies: ['/server:10230', '/onesync', 'oxmysql'],
+      dependencies: ['/server:10230', '/onesync', 'oxmysql', 'ox_core', 'ox_lib'],
       metadata: {
         server_only: 'yes'
       }
